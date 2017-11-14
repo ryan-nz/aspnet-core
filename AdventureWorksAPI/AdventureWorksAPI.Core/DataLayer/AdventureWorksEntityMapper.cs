a5f4992803f11d14cbe3e2b54f26a165b683146a
ryan-nz <yang.wang.nz@gmail.com>
2017-11-14 21:46:52 +1300

    ver0.3

---
+++ AdventureWorksAPI/AdventureWorksAPI.Core/DataLayer/AdventureWorksEntityMapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventureWorksAPI.Core.DataLayer
+{
+    public class AdventureWorksEntityMapper : EntityMapper
+    {
+    }
+}
---
+++ AdventureWorksAPI/AdventureWorksAPI.Core/DataLayer/IEntityMapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventureWorksAPI.Core.DataLayer
+{
+    interface IEntityMapper
+    {
+    }
+}
