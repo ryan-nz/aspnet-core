7d1dac192fedb66d093d2d02af8cf9153f6641aa
ryan-nz <yang.wang.nz@gmail.com>
2017-11-18 22:13:15 +1300

    delete the whole project.

--- AdventureWorksAPI/AdventureWorksAPI.Core/DataLayer/IEntityMap.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace AdventureWorksAPI.Core.DataLayer
-{
-    public interface IEntityMap
-    {
-        void Map(ModelBuilder modelBuilder);
-    }
-}
