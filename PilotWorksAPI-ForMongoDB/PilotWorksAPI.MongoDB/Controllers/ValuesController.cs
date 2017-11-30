9c27e219074926e705f4f5b7c2823b3fa0f086bb
ryan-nz <yang.wang.nz@gmail.com>
2017-11-30 18:12:13 +1300

    Changed the namespace

--- PilotWorksAPI-ForMongoDB/PilotWorksAPI.MongoDB/Controllers/ValuesController.cs
+++ PilotWorksAPI-ForMongoDB/PilotWorksAPI.MongoDB/Controllers/ValuesController.cs
@@ -1,10 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
-namespace PilotWorksAPI_ForMongoDB.Controllers
+namespace PilotWorksAPI.MongoDB.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
